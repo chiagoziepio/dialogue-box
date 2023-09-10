c2b5faf25eb4ba29e1071ff832011396178e1b5f
chiagozie pio <nduagubapio@gmail.com>
2023-09-10 12:45:21 +0100

    and our little project is ready

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
 const dialog = document.querySelector(".dialog");
 const subBtn = document.querySelector(".subBtn");
 const okBtn = document.querySelector(".okBtn");
+
+subBtn.addEventListener("click",()=>{
+    dialog.classList.add("show");
+});
+okBtn.addEventListener("click",()=>{
+    dialog.classList.remove("show");
+});
